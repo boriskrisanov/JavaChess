043c25c79a78518dd5efc3c275393256f11aa774
Boris Krisanov <boriskrisanov1@gmail.com>
2023-11-14 21:07:38 +0000

    Update move generation test

--- src/test/java/io/github/boriskrisanov/javachess/MoveGenerationTest.java
+++ src/test/java/io/github/boriskrisanov/javachess/MoveGenerationTest.java
@@ -59,12 +59,7 @@ public class MoveGenerationTest {
     }
 
     @Test
-    void testStartingPositionDepth5() {
-        assertEquals(4865609, runTest(5, Board.STARTING_POSITION_FEN));
-    }
-
-    @Test
-    void testDepth6() {
+    void testStartingPositionDepth6() {
         assertEquals(119060324, runTest(6, Board.STARTING_POSITION_FEN));
     }
 
@@ -80,26 +75,26 @@ public class MoveGenerationTest {
 
     @Test
     void testPosition1() {
-        assertEquals(15972, runTest(6, "5k1K/8/8/8/8/7p/6P1/8 w - - 0 1"));
+        assertEquals(193690690, runTest(5, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0"));
     }
 
     @Test
-    void testPosition3() {
+    void testPosition2() {
         assertEquals(11030083, runTest(6, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0"));
     }
 
     @Test
-    void testPosition4() {
+    void testPosition3() {
         assertEquals(15833292, runTest(5, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"));
     }
 
     @Test
-    void testPosition5() {
+    void testPosition4() {
         assertEquals(89941194, runTest(5, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"));
     }
 
     @Test
-    void testPosition6() {
+    void testPosition5() {
         assertEquals(164075551, runTest(5, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 "));
     }
 }
